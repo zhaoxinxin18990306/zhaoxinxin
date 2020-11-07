c7b414f24fb450920a8e7293d9a60f57324ef9b4
tianrui189 <72297307+tianrui189@users.noreply.github.com>
2020-11-07 10:26:46 +0800

    作业6
    作业6

---
+++ zuoye6/app/src/main/java/com/example/zuoye6/AppWriteActivity.java
@@ -0,0 +1,85 @@
+package com.example.zuoye6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.zuoye6.util.DateUtil;
+
+public class AppWriteActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText et_name;
+    private EditText et_age;
+    private EditText et_height;
+    private EditText et_weight;
+    private boolean bMarried = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_write);
+        et_name = findViewById(R.id.et_name);
+        et_age = findViewById(R.id.et_age);
+        et_height = findViewById(R.id.et_height);
+        et_weight = findViewById(R.id.et_weight);
+        findViewById(R.id.btn_save).setOnClickListener(this);
+
+        Spinner sp_married = findViewById(R.id.sp_married);
+        sp_married.setOnItemSelectedListener(new TypeSelectedListener());
+    }
+
+    class TypeSelectedListener implements AdapterView.OnItemSelectedListener {
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            bMarried = (arg2 == 0) ? false : true;
+        }
+
+        public void onNothingSelected(AdapterView<?> arg0) {
+        }
+    }
+    private String[] typeArray = {"未婚", "已婚"};
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_save) {
+            String name = et_name.getText().toString();
+            String age = et_age.getText().toString();
+            String height = et_height.getText().toString();
+            String weight = et_weight.getText().toString();
+            if (TextUtils.isEmpty(name)) {
+                showToast("请先填写姓名");
+                return;
+            } else if (TextUtils.isEmpty(age)) {
+                showToast("请先填写年龄");
+                return;
+            } else if (TextUtils.isEmpty(height)) {
+                showToast("请先填写身高");
+                return;
+            } else if (TextUtils.isEmpty(weight)) {
+                showToast("请先填写体重");
+                return;
+            }
+            // 获取当前应用的Application实例
+            MainApplication app = MainApplication.getInstance();
+            // 以下直接修改Application实例中保存的映射全局变量
+            app.mInfoMap.put("name", name);
+            app.mInfoMap.put("age", age);
+            app.mInfoMap.put("height", height);
+            app.mInfoMap.put("weight", weight);
+            app.mInfoMap.put("married", typeArray[!bMarried ? 0 : 1]);
+            app.mInfoMap.put("update_time", DateUtil.getNowDateTime("yyyy-MM-dd HH:mm:ss"));
+            showToast("数据已写入全局内存");
+
+            Intent intent = new Intent(this, AppReadActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void showToast(String desc) {
+        Toast.makeText(this, desc, Toast.LENGTH_SHORT).show();
+    }
+}
